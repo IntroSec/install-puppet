fb8e4d03cb8c8a6eff5acc37aba9f2811059a3b1
Andre Mattie <devel@introsec.ca>
2017-03-18 15:44:33 -0400

    added variables and basic commands Signed-off-by: Andre Mattie <devel@introsec.ca>

--- puppet_inst.sh
+++ puppet_inst.sh
@@ -13,11 +13,13 @@
 ################################################################################
 
 # variables
-ROOTUID="0"
 VERSION=$1
+SERVER=$2
+REPO="https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb"
+PACKAGE="puppetlabs-release-pc1-xenial.deb"
 
 # ensure script is being run by root
-if [ "$(id -u)" -ne "$ROOTUID" ] ; then
+if [ "$(id -u)" -ne "0" ] ; then
     echo "You must be root to run this script"
     exit 1
 fi
@@ -29,7 +31,14 @@ fi
 # update system
 apt-get update && apt-get upgrade -y
 
-#TODO add .deb download for puppet repo
+# download puppet repository package
+curl -O $REPO
+
+# install repository package
+dpkg -i $PACKAGE
+
+# update repositories
+apt-get update
 
 #TODO Add argument for puppetserver
 
